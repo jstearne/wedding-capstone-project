1e7d6efb0cdb9bc10b6e2b782c9ba6efa6b8691a
Jared Stearne <jared.stearne@gmail.com>
2021-11-06 11:09:08 -0700

    Finished refactoring. Test out and make sure Post CRUD is done!

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,10 +6,9 @@ urlpatterns = [
     path('', views.Home.as_view(), name="home"),
     path('guestbook/', views.Guestbook.as_view(), name="guestbook"),
 # Post CRUD functionality (Guestbook serves as READ)
-    path('guestbook/new/', views.CreatePost.as_view(), name="create_post"),
-    # Having issues with this route, update post!
-    path('guestbook/<int:pk>/update/', views.UpdatePost.as_view(), name="post_update"),
-    path('guestbook/<int:pk>/delete', views.DeletePost.as_view(), name="post_delete"),
+    path('guestbook/new/', views.CreatePost.as_view(), name="guestbook_create"),
+    path('guestbook/<int:pk>/update/', views.UpdatePost.as_view(), name="guestbook_update"),
+    path('guestbook/<int:pk>/delete', views.DeletePost.as_view(), name="guestbook_delete"),
 # remaining pages
     path('accommodations/', views.Accommodations.as_view(), name="accommodations"),
     path('schedule/', views.Schedule.as_view(), name="schedule"),
