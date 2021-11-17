02a9cc6ad3195d9182dcd179c7618334be5c437d
Jared Stearne <jared.stearne@gmail.com>
2021-11-17 08:11:40 -0800

    Added four error pages for 400-403-404-500 codes.

--- wedding_project/settings.py
+++ wedding_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ee7w^fpil7m5r99fvtghcpcs#es$%)ialw0eci)%zq^cu4(rn@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
